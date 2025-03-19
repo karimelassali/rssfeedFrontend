14c56ca37b13452ff31769822424eaeda9d418f0
Karim_kali <fifakarim52@gmail.com>
2025-03-19 11:36:55 +0100

    fixing description publishing

--- app/api/publish/[id]/route.js
+++ app/api/publish/[id]/route.js
@@ -3,15 +3,15 @@ import axios from "axios";
 
 export async function POST(request, { params }) {
     const { id } = params;
-    const {title , descreption , image,category,showInHomePage, publishType,date,time,scheduledTime} = await request.json();
-    console.log(title, descreption,image,category,showInHomePage, publishType,date,time,scheduledTime);
+    const {title , description , image,category,showInHomePage, publishType,date,time,scheduledTime} = await request.json();
+    console.log(title, description,image,category,showInHomePage, publishType,date,time,scheduledTime);
     try {
       const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}api/article/${id}/publish`,{
         headers: {
           "Content-Type": "application/json",
           'Authorization': `Bearer ${request.cookies.get('authToken')?.value}`,
         },
-        title, description:descreption,image,category,showInHomePage, publishType,date,time,scheduledTime
+        title, description, image, category, showInHomePage, publishType, date, time, scheduledTime
       });
       const data = response.data;
       return NextResponse.json(data);
